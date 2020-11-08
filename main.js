c95d565f84ee7f05e312f7457a0b6e3f7f32f16a
feinoko <feinoko@gmail.com>
2020-11-08 17:35:57 +0100

    update

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-let launchBox = document.querySelector('p');
+/* let launchBox = document.querySelector('p');
 
-launchBox.addEventListener('click', insertFunction);
+launchBox.addEventListener('click', insertFunction); */
 
 function insertFunction() {
-   let age = prompt('insert age');
+   /* let age = prompt('insert age');
    let time = prompt('did you register in morning or afternoon(type AM or PM) ?');
    let earlyReg = true;
 
@@ -18,8 +18,27 @@ function insertFunction() {
       alert('Your race starts at 1100 hrs since you are over 18 and registered late' + ` & your race number is ${raceNumberLate}`);
    } else {
       alert('enter valid age!');
-      insertFunction();
-   }
+      insertFunction(); */
+      
+      let age;
+      let age1 = document.getElementById("less").value;
+      let age2 = document.getElementById("more").value;
+
+      // checks if no radio button is filled by falsy check, function is then called again
+      if (age1 && age2 === false) {
+         alert('please inform us about your age');
+         insertFunction();
+      }
+
+      if (age1) {
+         age = document.getElementById("less").value;
+      } else {
+         age = document.getElementById("more").value;
+      }
+
+      alert(age);
+   
+   /* } */
 
 
 
